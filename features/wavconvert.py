7a26341ee26e3bf7c9b498f19e2dadbfbcf1e212
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-06-21 02:16:22 +0530

    mp3 to wav function

---
+++ features/wavconvert.py
@@ -0,0 +1,9 @@
+import envoy
+
+
+def mp3_to_wav(file_name):
+    wav_file_name = "".join(file_name.rsplit('.')[:-1:]) + ".wav"
+    cmd = "ffmpeg -i {0} {1}".format(file_name, wav_file_name)
+    e = envoy.run(cmd)
+    e.status_code
+    return e.status_code == 1
