1aab6969eb1d90be5c75f00c9a4ec747660fe3dc
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-06-21 02:23:37 +0530

    :bug:

--- features/wavconvert.py
+++ features/wavconvert.py
@@ -6,4 +6,5 @@ def mp3_to_wav(file_name):
     cmd = "ffmpeg -i {0} {1}".format(file_name, wav_file_name)
     e = envoy.run(cmd)
     e.status_code
-    return e.status_code == 1
+    return e.status_code == 0
+
