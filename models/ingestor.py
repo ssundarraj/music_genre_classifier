9f69903831db93ff6a961fc32a6a2f6894eba038
ajayrfhp <ajayrfhp1710@gmail.com>
2015-06-24 01:03:56 +0530

    removed venv

---
+++ models/ingestor.py
@@ -0,0 +1,26 @@
+import csv
+import numpy as np
+def get_data(file_name):
+    with open(file_name, 'rb') as csvfile:
+        data = []
+        target = []
+        datareader = csv.reader(csvfile, delimiter=',',)
+        for row in datareader:
+            data.append(row[:-1])
+
+
+
+            target.append(row[-1])
+
+    data_length = len(data)
+
+    split_factor = int(0.5*data_length)
+    training_data = data[:split_factor]
+    test_data = data[split_factor+1:]
+    training_target = target[:split_factor]
+    test_target = target[split_factor+1:]
+
+
+
+
+    return (training_data,training_target,test_data,test_target)        
